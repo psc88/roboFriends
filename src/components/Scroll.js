2694fc5ac8f719722349907dbeb4ead2577db0d7
Pablo Castillo <psc88sdrd@gmail.com>
2021-02-24 18:20:03 -0300

    Subida de la app

---
+++ src/components/Scroll.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return (
+        <div style={{ overflow: 'scroll', border: '5px solid black', height: '1000px'}}>
+            {props.children}
+        </div>
+    );
+};
+
+export default Scroll;
